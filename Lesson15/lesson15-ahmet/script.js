23e9afeffa1c48fcca6422d07c0b0ba1e76da6be
Ahmet YAYIK <ahmtyayik@gmail.com>
2025-12-13 22:22:29 +0300

    Update Lesson15/lesson15-ahmet/script.js
    Apply review suggestion for switch statement.
    
    Co-authored-by: Code2Career <info@annaatalay.com>

--- Lesson15/lesson15-ahmet/script.js
+++ Lesson15/lesson15-ahmet/script.js
@@ -587,27 +587,27 @@ console.log('Ex.24');
 function getSeason(monthNum) {
   switch (monthNum) {
     case 12:
-      case 1:
-        case 2:
-          console.log('Winter');
-          break;
-          case 3:
-            case 4:
-              case 5:
-                console.log('Spring');
-                break;
-                case 6:
-                  case 7:
-                    case 8:
-                      console.log('Summer');
-                      break;
-                      case 9:
-                        case 10:
-                          case 11:
-                            console.log('Autumn');
-                            break;
-                            default:
-                              console.log('Invalid month');
+    case 1:
+    case 2:
+      console.log('Winter');
+      break;
+    case 3:
+    case 4:
+    case 5:
+      console.log('Spring');
+      break;
+    case 6:
+    case 7:
+    case 8:
+      console.log('Summer');
+      break;
+    case 9:
+    case 10:
+    case 11:
+      console.log('Autumn');
+      break;
+    default:
+      console.log('Invalid month');
   }
 }
 
