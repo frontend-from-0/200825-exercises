93de852c60ba33463b7aecf3ca7253c7e264a8e9
Ahmet YAYIK <ahmtyayik@gmail.com>
2026-02-01 00:44:17 +0300

    Update Lesson14/ahmet/script.js
    Co-authored-by: Anna S. Atalay <annaatalay@hotmail.com>

--- Lesson14/ahmet/script.js
+++ Lesson14/ahmet/script.js
@@ -194,6 +194,6 @@ console.log('Ex.26', 'Multiplied value:', incrementedValue3);
 // 27. Declare a=2 and b=3. Increment a using the prefix ++ operator, then add b to the result and log it.
  let q = 2; 
  const incrementedValue4 = ++q;
- let w = 3;
+ const w = 3;
  let finalValue = incrementedValue4 + w;
  console.log('eX.27', 'final value is:', finalValue);
