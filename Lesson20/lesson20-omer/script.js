ed482db0eeeb0b7a1b97bd08e130b46febdcde45
OmerKocak0 <kocakomer0707@gmail.com>
2026-02-18 16:11:40 +0300

    Fixed corrections.

--- Lesson20/lesson20-omer/script.js
+++ Lesson20/lesson20-omer/script.js
@@ -12,18 +12,17 @@ const selectedName = document.getElementById('selected-name');
 const inputEmail = document.getElementById('email');
 const selectedMail = document.getElementById('selected-email');
 const confirmedName = document.getElementById('confirmed-name');
-const confirmedEmail = document.getElementById('confirmed-email')
-let timeoutName = null;
-let timeoutEmail = null;
+const confirmedEmail = document.getElementById('confirmed-email');
+
 
 function setMinDate() {
   const today = new Date();
   const tomorrow = new Date(today);
-  tomorrow.setDate(tomorrow.getDate() + 1); 
+  tomorrow.setDate(tomorrow.getDate() + 1);
   const year = tomorrow.getFullYear();
   const month = String(tomorrow.getMonth() + 1).padStart(2, '0');
   const day = String(tomorrow.getDate()).padStart(2, '0');
-  
+
   const minDate = `${year}-${month}-${day}`;
   dateInput.setAttribute('min', minDate);
 }
@@ -51,7 +50,7 @@ dateInput.addEventListener('change', function () {
 
 bookingForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  
+
   if (!data.date || !data.time || !data.username || !data.email) {
     return;
   }
@@ -81,38 +80,31 @@ function allowSubmit() {
     confirmButton.removeAttribute('disabled');
   }
   else {
-    confirmButton.setAttribute('disabled', 'true'); 
+    confirmButton.setAttribute('disabled', 'true');
   }
 }
-inputName.addEventListener('input', () => {
-  clearTimeout(timeoutName);
-  timeoutName = setTimeout(() => {
-  console.log("girdi var")
-    if(inputName.value.trim() !== ""){
-      selectedName.innerText = inputName.value;
-      data.username = inputName.value
-      
-    } 
-    else {
-      selectedName.innerText = "-";
-      data.username = null;
-    }
-    allowSubmit()
-  }, 500)
-})
-inputEmail.addEventListener('input', () => {
-  clearTimeout(timeoutEmail);
-  timeoutEmail = setTimeout(() => {
-  console.log("girdi var")
-    if(inputEmail.value.trim() !== ""){
-      selectedMail.innerText = inputEmail.value;
-      data.email = inputEmail.value
-      
-    } 
-    else {
-    selectedMail.textContent = '-';
-    data.email = null;
+inputName.addEventListener('change', () => {
+  if (inputName.value.trim() !== "") {
+    selectedName.innerText = inputName.value;
+    data.username = inputName.value;
+  } else {
+    selectedName.textContent = "-";
+    data.username = null;
+  }
+  allowSubmit();
+  inputEmail.addEventListener('input', () => {
+    clearTimeout(timeoutEmail);
+    timeoutEmail = setTimeout(() => {
+      console.log("girdi var")
+      if (inputEmail.value.trim() !== "") {
+        selectedMail.innerText = inputEmail.value;
+        data.email = inputEmail.value
 
-    }allowSubmit()
-  }, 500)
-})
+      }
+      else {
+        selectedMail.textContent = '-';
+        data.email = null;
+
+      } allowSubmit()
+    }, 500)
+  })})
