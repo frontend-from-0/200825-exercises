e6252e32c45fe8e00271e8102adc24d4088684d5
Omer Kocak <kocakomer0707@gmail.com>
2026-02-18 16:28:03 +0300

    Lesson21 Practical Assigment (#74)
    * Lesson21 Practical Assigment
    
    * Fixed

--- Lesson21/script.js
+++ Lesson21/script.js
@@ -3,7 +3,7 @@ const phonePattern = /^\+?\d(?:\s?\d){9,19}$/;
 const onlyLettersPattern = /^[a-zA-Z\s-]+$/;
 const cardPattern = /^(\d{4}\s){3}\d{4}$/;
 const expDatePattern = /^(0[1-9]|1[0-2])\/\d{2}$/;
-const cvvPattern = /^\d{3,4}$/;
+const cvvPattern = /^\d{3}/;
 
 
 const formElement = document.getElementById('checkoutForm');
@@ -17,29 +17,51 @@ const lastNameInput = document.getElementById('lastname');
 const lastNameError = document.getElementById('lastnameError');
 const cardNumberInput = document.getElementById('cardnumber');
 const cardNumberError = document.getElementById('cardnumberError');
-const expDateInput = document.getElementById('expDate');
+const expDate = document.getElementById('expDate');
 const expDateError = document.getElementById('expDateError');
 const cvvInput = document.getElementById('cvv');
 const cvvError = document.getElementById('cvvError');
-
 let formCorrect = true;
+cardNumberInput.setAttribute('maxlength', 19)
+
 
 emailInput.addEventListener('blur', validateEmail);
 phoneInput.addEventListener('blur', validatePhone);
-cardNumberInput.addEventListener('blur', validateCardNumber);
-expDateInput.addEventListener('blur', validateExpDate);
-cvvInput.addEventListener('blur', validateCVV);
-
 firstNameInput.addEventListener('blur', () =>
   validateName(firstNameInput, firstNameError),
 );
 lastNameInput.addEventListener('blur', () =>
   validateName(lastNameInput, lastNameError),
 );
-
+cardNumberInput.addEventListener('blur', validateCardNumber);
+cardNumberInput.addEventListener('input',(e) => {
+  let cardValue = e.target.value.replace(/\D/g,'');
+  let formattedCardValue = cardValue.replace(/(\d{4})(?=\d)/g, '$1 ');
+  e.target.value = formattedCardValue;
+})
+expDate.addEventListener('blur', validateExpDate);
+expDate.addEventListener('input', (dateValue) => {
+  let date = dateValue.target.value.replace(/\D/g,'').slice(0,4);
+  if(date.length>2){
+    
+    let month = parseInt(date.substring(0,2));
+    if(month > 12){
+      console.log('12den büyük')
+      date = '12' + date.substring(2)
+    }
+  }
+  let formattedDate = date.replace(/(\d{2})(?=\d)/g,'$1/');
+  dateValue.target.value = formattedDate;
+  
+})
+cvvInput.addEventListener('blur',validateCVV);
+cvvInput.addEventListener('input', (cvvNumber) => {
+  let userInputCVV = cvvNumber.target.value.replace(/\D/g,'').slice(0,3);
+  cvvNumber.target.value = userInputCVV
+})
 function validateEmail() {
   if (emailPattern.test(emailInput.value)) {
-    emailError.innerText = '';
+    emailError.textContent = '';
   } else {
     emailError.innerText =
       'Please enter correct email address (e.g. john@gmail.com)';
@@ -62,7 +84,7 @@ function validateName(input, errorField) {
     errorField.innerText = 'This field should contain less than 50 characters.';
     formCorrect = false;
   } else {
-    if (onlyLettersPattern.test(input.value)) {
+    if (onlyLettersPattern.test(input.value.trim())) {
       errorField.innerText = '';
     } else {
       errorField.innerText = 'This field can only contain letters.';
@@ -71,32 +93,34 @@ function validateName(input, errorField) {
   }
 }
 
-function validateCardNumber() {
-  if (cardPattern.test(cardNumberInput.value)) {
+function validateCardNumber(){
+  if(cardPattern.test(cardNumberInput.value)){
     cardNumberError.innerText = '';
-  } else {
-    cardNumberError.innerText =
-      'Please enter correct card number (e.g. 1234 5678 9101 1121)';
+  }
+  else {
+    cardNumberError.innerText = 'Incomplete or incorrect information.';
     formCorrect = false;
   }
-}
 
-function validateExpDate() {
-  if (expDatePattern.test(expDateInput.value)) {
+}
+function validateExpDate(){
+  if(expDatePattern.test(expDate.value)){
     expDateError.innerText = '';
-  } else {
-    expDateError.innerText =
-      'Please enter correct expiration date (e.g. 12/34)';
-    formCorrect = false;
+  }
+  else {
+    expDateError.innerText = 'Please enter valid expression date.'
+    formCorrect = false
   }
 }
 
-function validateCVV() {
-  if (cvvPattern.test(cvvInput.value)) {
+function validateCVV(){
+  if(cvvPattern.test(cvvInput.value)){
     cvvError.innerText = '';
-  } else {
-    cvvError.innerText = 'Please enter correct CVV (3 or 4 digits)';
-    formCorrect = false;
+    
+  }
+  else {
+    cvvError.innerText = 'Please enter CVV code which existing on behind your card.'
+    formCorrect = false
   }
 }
 
@@ -108,12 +132,16 @@ formElement.addEventListener('submit', function (event) {
   validatePhone();
   validateName(firstNameInput, firstNameError);
   validateName(lastNameInput, lastNameError);
-  validateCardNumber(cardNumberInput, cardNumberError);
-  validateExpDate(expDateInput, expDateError);
-  validateCVV(cvvInput, cvvError);
+  validateCardNumber();
+  validateExpDate();
+  validateCVV();
 
   if (formCorrect) {
     document.getElementById('success').classList.remove('hidden');
     formElement.classList.add('hidden');
   }
 });
+
+
+
+
