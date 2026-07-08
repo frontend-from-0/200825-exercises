2224949983aeb4832332360d0651e77e06e0751f
Ahmet YAYIK <ahmtyayik@gmail.com>
2026-07-09 02:39:57 +0300

    Add initial HTML, CSS, and JavaScript files for API Practical Assignment (#91)
    * Add initial HTML, CSS, and JavaScript files for API Practical Assignment
    
    * correct folder structure
    
    * Apply suggestion from @code2career
    
    Co-authored-by: Code2Career <info@annaatalay.com>
    
    ---------
    
    Co-authored-by: annasvst <annaatalay@hotmail.com>
    Co-authored-by: Code2Career <info@annaatalay.com>

---
+++ Lesson23/ahmet/script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("posts-container");
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((posts) => {
+    posts.forEach((post) => {
+      const postDiv = document.createElement("div");
+      postDiv.classList.add("post");
+
+      const title = document.createElement("h2");
+      title.classList.add("post-title");
+      title.textContent = post.title;
+
+      const body = document.createElement("p");
+      body.textContent = post.body;
+
+      postDiv.appendChild(title);
+      postDiv.appendChild(body);
+
+      container.appendChild(postDiv);
+    });
+  });
